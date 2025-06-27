c9093a2bddc71fab9056453e62a568566b4a8a69
SyndicateCID <charrodterrell@yahoo.com>
2025-06-27 18:51:47 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import WordCard from './components/WordCard/WordCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Dictionary App
       </header>
+     <main>
+      <WordCard en="translation" de="ubersetzung" />
+      <WordCard en="egg" de="ei" />
+
+     </main>
     </div>
   );
 }
---
+++ src/components/WordCard/WordCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './WordCard.css';
+
+export default function WordCard(props) {
+  return (
+    <div className="WordCard">
+        <p>English: {props.en}</p>
+        <p>German: {props.de}</p>
+      
+    </div>
+  )
+}
