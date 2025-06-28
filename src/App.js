e00f1d00a76884739f36fc8b66c13d36ca6259b4
SyndicateCID <charrodterrell@yahoo.com>
2025-06-27 19:26:30 -0500

    ?

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Dictionary App
+        Dictionary App
       </header>
      <main>
       <WordCard en="translation" de="ubersetzung" />
